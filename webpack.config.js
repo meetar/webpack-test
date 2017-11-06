63efb7b98508c9713be968254e2b5e49413f9367
Peter Richardson <peter@mapzen.com>
2017-11-06 13:45:07 -0800

    noParse

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   entry: './main.js',
   output: {
-    filename: 'bundle.js'
+    filename: './bundle.js'
+  },
+  module: {
+  	// Apply `noParse` to Tangram to prevent mangling of UMD boilerplate
+    noParse: /tangram\/dist\/tangram/
   }
 };
